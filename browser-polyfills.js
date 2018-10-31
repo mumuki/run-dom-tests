59eca32ab94ae11d8678607baeff46237d9b3183
Rodrigo Alfonso <rodri042@gmail.com>
2018-10-30 22:21:48 -0300

    Adding mocked prompt and confirm

--- browser-polyfills.js
+++ browser-polyfills.js
@@ -2,5 +2,7 @@ module.exports = {
   XMLHttpRequest: require("xmlhttprequest"),
   btoa: require("btoa"),
   atob: require("atob"),
-  alert: (msg) => { global._last_alert_message = msg; }
+  alert: (msg) => { global._last_alert_message_ = msg; },
+  confirm: () => global._confirm_response_,
+  prompt: () => global._prompt_response_
 }
