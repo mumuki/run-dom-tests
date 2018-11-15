08e222a4cf61310d7609a0ff1382176715df74b7
Rodrigo Alfonso <rodri042@gmail.com>
2018-11-15 03:35:51 -0300

    Improving alert, prompt, confirm polyfills

--- browser-polyfills.js
+++ browser-polyfills.js
@@ -3,6 +3,12 @@ module.exports = {
   btoa: require("btoa"),
   atob: require("atob"),
   alert: (msg) => { global._last_alert_message_ = msg; },
-  confirm: () => global._confirm_response_,
-  prompt: () => global._prompt_response_
+  confirm: (msg) => {
+    global._last_confirm_message_ = msg;
+    return global._confirm_response_;
+  },
+  prompt: (msg) => {
+    global._last_prompt_message_ = msg;
+    return global._prompt_response_;
+  }
 }
