8661bf7d5bcd643bf19042245cae41c7e65e26a8
Franco Leonardo Bulgarelli <franco@mumuki.org>
2021-03-23 16:43:11 -0300

    Add tests

--- tests/events.js
+++ tests/events.js
@@ -5,9 +5,13 @@ module.exports = {
     <title>events</title>
   </head>
   <body>
-    <button>Say hello</button>
+    <button id="alert">Alert</button>
+    <button id="confirm">Confirm</button>
+    <button id="prompt">Prompt</button>
     <script>
-      document.querySelector("button").addEventListener("click", () => alert("Hi!"));
+      document.querySelector("#alert").addEventListener("click", () => alert("Hi!"));
+      document.querySelector("#confirm").addEventListener("click", () => confirm("Are you sure?"));
+      document.querySelector("#prompt").addEventListener("click", () => prompt("Please tell us your name"));
     </script>
   </body>
 </html>
@@ -16,9 +20,21 @@ module.exports = {
   "tests": `
 describe("events", function() {
   it("shows an alert when the button is clicked", function() {
-    _dispatch_("click", document.querySelector("button"));
+    _dispatch_("click", document.querySelector("#alert"));
 
     _last_alert_message_.should.eql("Hi!");
+  });
+
+  it("shows a confirm when the button is clicked", function() {
+    _dispatch_("click", document.querySelector("#confirm"));
+
+    _last_confirm_message_.should.eql("Are you sure?");
+  });
+
+  it("shows a promt when the button is clicked", function() {
+    _dispatch_("click", document.querySelector("#prompt"));
+
+    _last_prompt_message_.should.eql("Please tell us your name");
   });
 });
 `};
